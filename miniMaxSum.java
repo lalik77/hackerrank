94f5c0e23d308208dd0b52f327d17e2692ec080a
Alexander Mami <45859153+lalik77@users.noreply.github.com>
2019-12-07 19:05:48 +0300

    Update miniMaxSum.java

--- miniMaxSum.java
+++ miniMaxSum.java
@@ -1,1 +1,26 @@
+static void miniMaxSum(int[] arr) {
 
+        //Using Arrays.sort method to Find Maximum and Minimum Values in an Array
+
+        Arrays.sort(arr);
+
+        long  minsum=0;
+        long maxsum=0;
+
+        for (int i=0;i<arr.length-1;i++) {
+
+            minsum+=arr[i];
+        }
+
+        for (int i=1;i<arr.length;i++) {
+
+            maxsum+=arr[i];
+        }
+
+
+        System.out.println("Minimum = " + arr[0]);
+        System.out.println("Minimum = " + arr[arr.length-1]);
+
+        System.out.println( "MinSum = " + minsum + " MaxSum = " + maxsum );
+
+    }
